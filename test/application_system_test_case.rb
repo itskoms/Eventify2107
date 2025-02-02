f1e0cb7967d45b5ccd750ee245bfdb24d2e9a99a
Parsa Hemmati <parsahm12@gmail.com>
2025-02-02 12:47:17 -0500

    added front end test + front end test simulation using gems + coverage rpeort.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+
+  def setup
+    Capybara.app_host = "http://localhost"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,14 @@
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/test/"
+  add_filter "/config/"
+  add_group "Controllers", "app/controllers"
+  add_group "Models", "app/models"
+  add_group "Views", "app/views"
+  add_group "Helpers", "app/helpers"
+  add_group "JavaScript", "app/javascript"
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
